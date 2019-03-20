62b2acbaee2f2c9d97ecd39492f02c15616bd8fe
manybooks <manybooks.0703@gmail.com>
2019-03-20 19:43:16 +0900

    Add view model class

--- EruptRecorder/Settings/Settings.cs
+++ EruptRecorder/Settings/Settings.cs
@@ -1,12 +1,34 @@
 ﻿using System;
 using System.IO;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace EruptRecorder.Settings
 {
+    public class SettingsViewModel
+    {
+        public ObservableCollection<CopySetting> copySettings { get; set; }
+        public RecordingSetting recordingSetting { get; set; }
+        public LoggingSetting loggingSetting { get; set; }
+
+        public SettingsViewModel()
+        {
+            // TODO: デバッグ用コード。あとで消す。
+            this.copySettings = new ObservableCollection<CopySetting>
+            {
+                new CopySetting(true, 1, "srcDir1", "destDir1", "aaa", ".img"),
+                new CopySetting(true, 2, "srcDir2", "destDir2", "bbb", ".jpg"),
+                new CopySetting(false, 3, "srcDir3", "destDir3", "ccc", ".*"),
+                new CopySetting(true, 4, "srcDir4", "destDir4", "ddd", ".txt"),
+            };
+            this.recordingSetting = new RecordingSetting(5);
+            this.loggingSetting = new LoggingSetting("log/");
+        }
+    }
+
     public class CopySetting
     {
         public bool isActive { get; set; }
