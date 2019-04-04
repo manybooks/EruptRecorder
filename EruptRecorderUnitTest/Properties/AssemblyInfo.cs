7862f3dda1658a7984a922755badc977bfc5ff7b
manybooks <manybooks.0703@gmail.com>
2019-04-04 22:12:32 +0900

    Add unit test

---
+++ EruptRecorderUnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("EruptRecorderUnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("EruptRecorderUnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("697cc7e7-35b8-4865-b6ce-5eb865f0d269")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ EruptRecorderUnitTest/Settings/SettingsTest.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using EruptRecorder.Settings;
+
+namespace EruptRecorderUnitTest.Settings
+{
+    [TestClass]
+    public class SettingsTest
+    {
+        [DataRow(true, 1, "jpg", @"C:\Users\user\storage", @"C:\Users\user\storage")]
+        [DataRow(true, 1, "bmp", @"C:\Users\user\storage", @"C:\Users\user\storage")]
+        [DataRow(true, 1, "abcde", @"C:\Users\user\storage", @"C:\Users\user\storage")]
+        [DataRow(true, 1, "*", @"C:\Users\user\storage", @"C:\Users\user\storage")]
+        [TestMethod]
+        public void FileExtensionValidationTest(bool isActive, int index, string fileExtension, string srcDir, string destDir)
+        {
+            CopySetting copySetting = new CopySetting();
+            copySetting.isActive = isActive;
+            copySetting.index = index;
+            copySetting.fileExtension = fileExtension;
+            copySetting.srcDir = srcDir;
+            copySetting.destDir = destDir;
+
+            Assert.IsTrue(copySetting.IsValid());
+        }
+
+        [DataRow(true, 1, "Jpg", @"C:\Users\user\storage", @"C:\Users\user\storage")]
+        [DataRow(true, 1, ".bmp", @"C:\Users\user\storage", @"C:\Users\user\storage")]
+        [DataRow(true, 1, "あ", @"C:\Users\user\storage", @"C:\Users\user\storage")]
+        [DataRow(true, 1, "123", @"C:\Users\user\storage", @"C:\Users\user\storage")]
+        [DataRow(true, 1, "a*", @"C:\Users\user\storage", @"C:\Users\user\storage")]
+        [DataRow(true, 1, "", @"C:\Users\user\storage", @"C:\Users\user\storage")]
+        [DataRow(true, 1, null, @"C:\Users\user\storage", @"C:\Users\user\storage")]
+        [TestMethod]
+        public void FileExtensionValidationWithInvalidArgsTest(bool isActive, int index, string fileExtension, string srcDir, string destDir)
+        {
+            CopySetting copySetting = new CopySetting();
+            copySetting.isActive = isActive;
+            copySetting.index = index;
+            copySetting.fileExtension = fileExtension;
+            copySetting.srcDir = srcDir;
+            copySetting.destDir = destDir;
+
+            Assert.ThrowsException<InvalidSettingsException>(() => copySetting.IsValid());
+        }
+    }
+}
